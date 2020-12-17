e959cf945c6d50dcc35071416fedc3aad810b0f5
madrackwp <52032916+madrackwp@users.noreply.github.com>
2020-12-17 10:41:40 +0800

    finished phonebook p6

--- part2/sandbox/src/App.js
+++ part2/sandbox/src/App.js
@@ -1,10 +1,24 @@
-import React,{ useState } from 'react'
+import axios from 'axios'
+import React,{ useState, useEffect } from 'react'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note...')
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  useEffect(hook, [])
+  console.log('render', notes.length, 'notes')
+
   
   const addNote = (event) => {
     event.preventDefault()
