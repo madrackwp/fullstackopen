92157b201af54fb492cbaadecb336c534e6b8b06
unknown <gohweipin98@gmail.com>
2020-12-18 21:15:25 +0800

    Fixed?

--- part3/sandbox3/index.js
+++ part3/sandbox3/index.js
@@ -95,7 +95,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
